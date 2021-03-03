e74a45a851cabc6155825c55741957d23c556fb2
sshres22 <sshres22@cougar.uh.edu>
2021-03-03 11:15:41 -0600

    complete

---
+++ Zylab9.10frequency.py
@@ -0,0 +1,14 @@
+import csv
+csv_file = input()
+my_dict = {}
+
+with open(csv_file) as file:
+    csv_reader = csv.reader(file)
+    for i in csv_reader:
+        for j in i:
+            if j in my_dict:
+                my_dict[j] = my_dict[j]+1
+            else:
+                my_dict[j] = 1
+for k, v in my_dict.items():
+    print(k,v)
